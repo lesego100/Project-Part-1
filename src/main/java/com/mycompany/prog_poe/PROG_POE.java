40e913ef6bca375020284944b9a29f845d652d43
Student <Student@LabVM2047644.dqiiskzfn5jexkowjpmpv3cqih.jnbx.internal.cloudapp.net>
2026-04-12 16:59:46 +0200

    project part 1 with edited reference order

--- src/main/java/com/mycompany/prog_poe/PROG_POE.java
+++ src/main/java/com/mycompany/prog_poe/PROG_POE.java
@@ -1,9 +1,9 @@
 /*
 REFERENCES:
-[1]Ramesh Fadatare, “Building a Login and Registration System with Java,” Javaguides.net, Apr. 07, 2024. https://www.javaguides.net/2024/04/building-login-and-registration-system-with-java.html (accessed Apr. 03, 2026).
-[2]Emeris School of Computer Science, “PROG5121 Unit Testing Getting started,” YouTube, Apr. 09, 2025. https://www.youtube.com/watch?v=MOhiM2SXZl0 (accessed Apr. 10, 2026).
-‌[3]IIEVC School of Computer Science, “PROG5121 Maven Automated testing,” YouTube, May 14, 2024. https://www.youtube.com/watch?v=oz0Qd5H4Onk (accessed Apr. 10, 2026).
-‌[4]J. Farrell, Java Programming., 10th ed. S.L.: Cengage Learning, 2022.
+[1]Emeris School of Computer Science, “PROG5121 Unit Testing Getting started,” YouTube, Apr. 09, 2025. https://www.youtube.com/watch?v=MOhiM2SXZl0 (accessed Apr. 10, 2026).
+‌[2]IIEVC School of Computer Science, “PROG5121 Maven Automated testing,” YouTube, May 14, 2024. https://www.youtube.com/watch?v=oz0Qd5H4Onk (accessed Apr. 10, 2026).
+‌[3]J. Farrell, Java Programming., 10th ed. S.L.: Cengage Learning, 2022.
+[4]Ramesh Fadatare, “Building a Login and Registration System with Java,” Javaguides.net, Apr. 07, 2024. https://www.javaguides.net/2024/04/building-login-and-registration-system-with-java.html (accessed Apr. 03, 2026).
 */
 package com.mycompany.prog_poe;
 public class PROG_POE {
