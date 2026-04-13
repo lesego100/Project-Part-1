5dc8f59b5cbf4824f05424710b8c5c93b20de530
Student <Student@LabVM2047644.dqiiskzfn5jexkowjpmpv3cqih.jnbx.internal.cloudapp.net>
2026-04-13 12:23:43 +0200

    project part 1 with edited

--- src/main/java/com/mycompany/prog_poe/REFERENCES.java
+++ src/main/java/com/mycompany/prog_poe/REFERENCES.java
@@ -1,8 +1,11 @@
 /*
 REFERENCES:
-[1]Emeris School of Computer Science, “PROG5121 Unit Testing Getting started,” YouTube, Apr. 09, 2025. https://www.youtube.com/watch?v=MOhiM2SXZl0 (accessed Apr. 10, 2026).
-‌[2]IIEVC School of Computer Science, “PROG5121 Maven Automated testing,” YouTube, May 14, 2024. https://www.youtube.com/watch?v=oz0Qd5H4Onk (accessed Apr. 10, 2026).
-‌[3]J. Farrell, Java Programming., 10th ed. S.L.: Cengage Learning, 2022.
-[4]Ramesh Fadatare, “Building a Login and Registration System with Java,” Javaguides.net, Apr. 07, 2024. https://www.javaguides.net/2024/04/building-login-and-registration-system-with-java.html (accessed Apr. 03, 2026).
+[1]baeldung, “Baeldung,” Baeldung on Kotlin, Oct. 09, 2016. https://www.baeldung.com/regular-expressions-java (accessed Mar. 28, 2026)
+[2]Emeris School of Computer Science, “PROG5121 Unit Testing Getting started,” YouTube, Apr. 09, 2025. https://www.youtube.com/watch?v=MOhiM2SXZl0 (accessed Apr. 10, 2026).
+[3]Geekific, “What is RegEx? | Introduction to Regexes | Regular Expressions and Java Patterns | Geekific,” YouTube, Jul. 07, 2021. https://www.youtube.com/watch?v=HvryGBPmNl8
+‌[4]IIEVC School of Computer Science, “PROG5121 Maven Automated testing,” YouTube, May 14, 2024. https://www.youtube.com/watch?v=oz0Qd5H4Onk (accessed Apr. 10, 2026).
+‌[5]J. Farrell, Java Programming., 10th ed. S.L.: Cengage Learning, 2022.
+[6]Ramesh Fadatare, “Building a Login and Registration System with Java,” Javaguides.net, Apr. 07, 2024. https://www.javaguides.net/2024/04/building-login-and-registration-system-with-java.html (accessed Apr. 03, 2026).
+[7]W3Schools, “Java Classes and Objects,” W3schools.com, 2019. https://www.w3schools.com/java/java_classes.asp (accessed Mar. 20, 2026)
 */
 
