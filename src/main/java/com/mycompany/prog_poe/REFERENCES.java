21af55e9e468c059836b87cd474b181e3a36b6de
Student <Student@LabVM2047644.dqiiskzfn5jexkowjpmpv3cqih.jnbx.internal.cloudapp.net>
2026-06-13 19:12:58 +0200

    part 3 edited

--- src/main/java/com/mycompany/prog_poe/REFERENCES.java
+++ src/main/java/com/mycompany/prog_poe/REFERENCES.java
@@ -7,5 +7,11 @@ REFERENCES:
 ‌[5]J. Farrell, Java Programming., 10th ed. S.L.: Cengage Learning, 2022.
 [6]Ramesh Fadatare, “Building a Login and Registration System with Java,” Javaguides.net, Apr. 07, 2024. https://www.javaguides.net/2024/04/building-login-and-registration-system-with-java.html (accessed Apr. 03, 2026).
 [7]W3Schools, “Java Classes and Objects,” W3schools.com, 2019. https://www.w3schools.com/java/java_classes.asp (accessed Mar. 20, 2026)
+[8]GeeksforGeeks, “Arrays in Java,” GeeksforGeeks, Dec. 21, 2016. https://www.geeksforgeeks.org/java/arrays-in-java/ (accessed May 15, 2026).
+[9]W3Schools, “Java Arrays,” W3schools.com, 2019. https://www.w3schools.com/java/java_arrays.asp (accessed May 15, 2026).
+[10]“Java Array: Declare, Initialize and Access Array With Examples,” Programiz.com, 2019. https://www.programiz.com/java-programming/arrays (accessed May 17, 2026).
+[11]W3Schools, “Java Switch,” www.w3schools.com, 2025. https://www.w3schools.com/java/java_switch.asp (accessed May 19, 2026).
+[12]GeeksforGeeks, “Switch Statements in Java,” GeeksforGeeks, May 15, 2017. https://www.geeksforgeeks.org/java/switch-statement-in-java/ (accessed May 21, 2026).
+[13]M. E. Naqvi, “How to Populate an Array in Java,” Delft Stack, Jun. 02, 2021. https://www.delftstack.com/howto/java/populate-an-array-in-java/ (accessed May 28, 2026).
 */
 
